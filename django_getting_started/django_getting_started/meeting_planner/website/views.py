f7451b8a65de041d8fef47cf38375027b76e95f0
lucasSerralha <lucas.serralha@gmail.com>
2022-03-03 17:18:01 -0300

    Build: Commit the django app I made
    First Django App. I made in a virtual machine, so i'm commiting it all.

---
+++ django_getting_started/django_getting_started/meeting_planner/website/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+from meetings.models import Meeting
+
+
+def welcome(request):
+    return render(request, "website/welcome.html",
+                  {"meetings": Meeting.objects.all()})
+
+
+def date(request):
+    return HttpResponse("This page was served at " + str(datetime.now()))
+
+
+def about(request):
+    return HttpResponse("I'm Lucas and I'm learning Django!")
